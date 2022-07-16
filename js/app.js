a8856ed274959c0fe6cc2b264116255f779102ab
Alfredo <fredy_rios0123@outlook.com>
2022-07-15 17:34:13 -0700

    I solved Third-Problem

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ Write a function called sum() that takes in two numbers as arguments and then re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
-function sum(a, b) { //eslint-disable-line
+function sum(a, b,) { //eslint-disable-line
   let sum = a + b;
   let total = `The sum of ${a} and ${b} is 11.`;
   let arr = [sum, total];
@@ -29,10 +29,10 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-  let multiplication = a * b;
+function multiply(a, b,) { //eslint-disable-line
+  let multiply = a * b;
   let message = `The product of ${a} and ${b} is 45.`;
-  let arr = [multiplication, message];
+  let arr = [multiply, message];
   return arr;
 }
 
@@ -54,11 +54,18 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+  let sumOne = sum (a, b) [0];
+  let multiplyOne = multiply (a, b) [0];
+  let sumTwo = sum (sumOne, c) [0];
+  let multiplyTwo = multiply (multiplyOne, c) [0];
+  let sumMessage = `${a} and ${b} and ${c} sum to ${sumTwo}.`;
+  let multiplyMessage = `The product of ${a} and ${b} and ${c} is ${multiplyTwo}.`
+  let sumAndMultiplyArr = [sumTwo, multiplyTwo, sumMessage, multiplyMessage];
+  return sumAndMultiplyArr;
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
